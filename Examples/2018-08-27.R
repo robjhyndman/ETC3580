ca30235edbd9692ee0301c6fc4a2e55ab025a8f6
Rob J Hyndman <robjhyndman@gmail.com>
2018-08-26 15:36:18 +1000

    Updated for week 6

---
+++ Examples/2018-08-27.R
@@ -0,0 +1,61 @@
+library(faraway)
+library(tidyverse)
+library(visreg)
+
+# Insurance example with offset
+help(motorins)
+motori <- as_tibble(motorins)
+motori
+summary(motori)
+motori <- motori %>%
+  mutate(Kilometres = as.numeric(Kilometres)) %>%
+  filter(Zone == 1)
+summary(motori)
+
+GGally::ggpairs(motori)
+
+motori %>%
+  mutate(Payment = log(Payment),
+         Insured= log(Insured)) %>%
+  GGally::ggpairs()
+
+# Log normal model
+llm <- lm(log(Payment/Insured) ~ Kilometres + Make + Bonus, motori)
+summary(llm)
+
+# Equivalently
+llg <- glm(
+  log(Payment) ~ offset(log(Insured)) + Kilometres + Make + Bonus,
+  family=gaussian ,  motori)
+summary(llg)
+
+# Gamma model
+gl <- glm(
+  Payment ~ offset(log(Insured)) + Kilometres + Make + Bonus,
+  family=Gamma(link=log), motori)
+summary(gl)
+
+
+# Tweedie model
+help(chredlin)
+as_tibble(chredlin)
+GGally::ggpairs(chredlin)
+chredlin %>% count(involact)
+
+library(mgcv)
+twmod <- gam(involact ~ race + fire + theft + age + log(income),
+             family=tw(link="log"), data=chredlin)
+summary(twmod)
+
+# Plot density with mass at zero:
+xgrid <- seq(1e-10, 1.25, len=100)
+p <- 1.108
+phi <- 0.30847
+(mu <- twmod$fit[1])
+(poismean <- mu^(2-p)/(phi * (2- p)))
+(p0 <- exp(-poismean))
+twden <- exp(ldTweedie(xgrid, mu, p=p, phi=phi)[,1])
+plot(xgrid, twden*(1-p0), type="l",xlab="x",ylab="Density")
+dmax <- max(twden*(1-p0))
+segments(0,0,0,dmax,lwd=2)
+text(0.08,dmax,paste0("p=",signif(p0,3)))
